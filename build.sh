5f028aad6ae975e0718bec494275928d38cc808a
jinjor <jinjorweb@gmail.com>
2019-10-05 23:01:53 +0900

    use --optimize

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ echo "built with original version"
 
 ./build-virtual-dom.sh
 rm -rf ./elm-stuff
-env ELM_HOME=`pwd` npx elm make src/Simple/Element.elm src/Simple/Application.elm --output=public/js/simple-patched.js
-env ELM_HOME=`pwd` npx elm make src/Extensions/Element.elm src/Extensions/Application.elm --output=public/js/extensions-patched.js
+env ELM_HOME=`pwd` npx elm make --optimize src/Simple/Element.elm src/Simple/Application.elm --output=public/js/simple-patched.js
+env ELM_HOME=`pwd` npx elm make --optimize src/Extensions/Element.elm src/Extensions/Application.elm --output=public/js/extensions-patched.js
 echo "built with patched version"
