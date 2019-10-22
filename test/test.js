832e4205f8bac47997bae6731edee517c9aeb440
jinjor <jinjorweb@gmail.com>
2019-10-22 19:41:52 +0900

    fix dirty solution

--- test/test.js
+++ test/test.js
@@ -1497,7 +1497,7 @@ describe("Simple", function() {
                 await assertCount(page, ".ext", 0);
               }
             }
-            describe.only("Boundary", function() {
+            describe("Boundary", function() {
               it("prepend .ext to body and prepend text", async function() {
                 await testBoundary("#boundary1", true);
               });
